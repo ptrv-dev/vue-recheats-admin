461a06b9ad0339da23149f6a04a1c309cb5d9639
ptrv-dev <petrov99119@gmail.com>
2023-01-29 20:36:39 +0200

    router configed

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import HomeView from '@/views/HomeView.vue';
+import PostsView from '@/views/PostsView.vue';
+import CategoriesView from '@/views/CategoriesView.vue';
+import UsersView from '@/views/UsersView.vue';
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView,
+  },
+  {
+    path: '/posts',
+    name: 'posts',
+    component: PostsView,
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    component: CategoriesView,
+  },
+  {
+    path: '/users',
+    name: 'users',
+    component: UsersView,
   },
 ];
 
