c6eaf662b8183b0064b6aad03ba84bcfc2a600c3
ptrv-dev <petrov99119@gmail.com>
2023-01-29 20:34:22 +0200

    aside

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
-  <router-view />
+  <app-menu />
+  <main id="main"><router-view /></main>
 </template>
+
+<script>
+import AppMenu from '@/components/AppMenu.vue';
+
+export default {
+  components: {
+    AppMenu,
+  },
+};
+</script>
 
 <style lang="scss">
 @import '@/scss/vars';
@@ -28,21 +39,29 @@ button {
   cursor: pointer;
 }
 h1 {
-  font-size: 2.5rem;
+  font-size: 2.5rem; /* 40px */
 }
 h2 {
-  font-size: 2rem;
+  font-size: 2rem; /* 32px */
 }
 h3 {
-  font-size: 1.5rem;
+  font-size: 1.5rem; /* 24px */
 }
 h4 {
-  font-size: 1.25rem;
+  font-size: 1.25rem; /* 20px */
 }
 h5 {
-  font-size: 1.125rem;
+  font-size: 1.125rem; /* 18px */
 }
 h6 {
-  font-size: 1rem;
+  font-size: 1rem; /* 16px */
+}
+#app {
+  min-height: 100vh;
+  display: flex;
+}
+#main {
+  padding: 2rem;
+  overflow: auto;
 }
 </style>
---
+++ src/components/AppMenu.vue
@@ -0,0 +1,86 @@
+<template>
+  <aside class="aside">
+    <h3 class="aside__logo">Admin-panel</h3>
+    <nav class="aside__nav aside-nav">
+      <router-link
+        v-for="(item, idx) in navigation"
+        :key="idx"
+        :to="item.link"
+        class="aside-nav__item"
+        active-class="aside-nav__item_active"
+        exact-active-class=""
+      >
+        <img :src="`/assets/img/icons/${item.icon}`" alt="" />
+        {{ item.title }}
+      </router-link>
+    </nav>
+  </aside>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navigation: [
+        {
+          icon: 'home.svg',
+          title: 'Home',
+          link: '/',
+          level: 1,
+        },
+        {
+          icon: 'folder.svg',
+          title: 'Posts',
+          link: '/posts',
+          level: 1,
+        },
+        {
+          icon: 'home.svg',
+          title: 'Categories',
+          link: '/categories',
+          level: 2,
+        },
+        {
+          icon: 'users.svg',
+          title: 'Users',
+          link: '/users',
+          level: 2,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/vars';
+.aside {
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  background: $color-grey;
+  min-width: 20rem;
+  &__logo {
+    align-self: center;
+    text-transform: uppercase;
+    margin-bottom: 2rem;
+  }
+}
+.aside-nav {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  &__item {
+    display: inline-flex;
+    align-items: center;
+    text-decoration: none;
+    color: $color-white;
+    padding: 0.5rem 1rem;
+    border-radius: 0.5rem;
+    gap: 0.5rem;
+    &_active {
+      background: $color-dark;
+    }
+  }
+}
+</style>
