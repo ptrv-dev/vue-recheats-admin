9a5062bd9b75b5c0e79ab92f32d4137867c8d1c8
ptrv-dev <petrov99119@gmail.com>
2023-01-30 18:38:26 +0200

    user form

---
+++ src/components/UserForm.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="user-form">
+    <h4 class="user-form__title">Create new user</h4>
+    <app-input
+      @input="
+        $emit('update:modelValue', {
+          ...modelValue,
+          username: $event.target.value,
+        })
+      "
+      placeholder="username..."
+    />
+    <app-input
+      @input="
+        $emit('update:modelValue', {
+          ...modelValue,
+          password: $event.target.value,
+        })
+      "
+      placeholder="password..."
+    />
+    <app-input
+      @input="
+        $emit('update:modelValue', {
+          ...modelValue,
+          link: $event.target.value,
+        })
+      "
+      placeholder="link..."
+    />
+    <app-button v-if="!modelValue.id" @click="$emit('create')"
+      >Create</app-button
+    >
+    <div class="user-form__row" v-else>
+      <app-button @click="$emit('edit')">Save</app-button>
+      <app-button @click="$emit('cancel')">Cancel</app-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppInput from './AppInput.vue';
+import AppButton from './AppButton.vue';
+export default {
+  components: { AppButton, AppInput },
+  props: {
+    modelValue: {
+      type: Object,
+      required: true,
+    },
+  },
+  emits: ['update:modelValue', 'create', 'edit', 'cancel'],
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/vars';
+.user-form {
+  padding: 1rem;
+  border-radius: 1rem;
+  background: $color-grey;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: center;
+  .input {
+    width: 20rem;
+  }
+  &__title {
+    font-weight: normal;
+  }
+  &__row {
+    display: flex;
+    justify-content: center;
+    gap: 1rem;
+  }
+}
+</style>
--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -1,3 +1,31 @@
 <template>
-  <h1>Users Page</h1>
+  <div class="users-view">
+    <user-form v-model="user" />
+  </div>
 </template>
+
+<script>
+import UserForm from '@/components/UserForm.vue';
+
+export default {
+  components: { UserForm },
+  data() {
+    return {
+      user: {
+        id: '',
+        username: '',
+        password: '',
+        link: '',
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.users-view {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+</style>
